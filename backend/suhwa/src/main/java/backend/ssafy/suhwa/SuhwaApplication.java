9e207b6c3faa4e03ec250faac9399f7df6f7a3a2
김선미 <hariaus@naver.com>
2026-07-21 13:20:54 +0900

    feat: 공통 타임스탬프/예외 처리 인프라 추가
    - BaseTimeEntity(createdAt/updatedAt, JPA Auditing) 추가, SuhwaApplication에
      @EnableJpaAuditing 등록
    - BusinessException + ErrorCode(HTTP 상태 매핑) + GlobalExceptionHandler로
      서비스 계층 예외를 계약(contracts/*.yaml)된 응답 형식(code/message)으로 일원화

--- backend/suhwa/src/main/java/backend/ssafy/suhwa/SuhwaApplication.java
+++ backend/suhwa/src/main/java/backend/ssafy/suhwa/SuhwaApplication.java
@@ -2,7 +2,9 @@ package backend.ssafy.suhwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SuhwaApplication {
 
