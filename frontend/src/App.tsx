40f12b66e666641b6c7c894ccaf28279fedf1234
오규연 <59raphyy@gmail.com>
2026-07-22 09:27:21 +0900

    feat: 메인페이지 레이아웃

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainPage } from "./features/home/pages/MainPage"; 
 import { StandaloneGameHarness } from "./game";
 
 export function App() {
-  return <StandaloneGameHarness />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* '/' 주소(기본 메인 주소)로 들어오면 MainPage를 보여줌 */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* 단독 게임 테스트 주소 (예: /game) */}
+        <Route path="/game" element={<StandaloneGameHarness />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
